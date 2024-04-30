4be347a48cd7f2a042e16c0764f3c23b87871f43
Salq24 <qogheneochuko@gmail.com>
2024-04-30 07:49:31 -0700

    sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -6,5 +6,5 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIViLEGES;
 --grants all privileges
-GRANT ALL PRIVILEGES ON hbnb_test_db.8 TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
