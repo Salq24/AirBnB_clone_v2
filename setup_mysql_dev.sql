3dc57236cbd9182cf8fb5cce7ae49c93ef43fc06
Salq24 <qogheneochuko@gmail.com>
2024-04-30 07:55:29 -0700

    sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,11 +1,11 @@
---This prepares a mysql server for the project
---creates a database
+-- This prepares a mysql server for the project
+-- creates a database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
---creates a new user
+-- creates a new user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
---Grants all privileges to the user
+-- Grants all privileges to the user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
---Grants select privilege for a user
+-- Grants select privilege for a user
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
