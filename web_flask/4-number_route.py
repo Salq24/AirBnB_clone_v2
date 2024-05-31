2b0b0819ab6f4024624a645f0f16517b9d4e8b2c
Salq24 <qogheneochuko@gmail.com>
2024-05-31 08:23:48 -0700

    hello_route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""This script starts a flask web app, listening
+on 0.0.0.0, port 5000
+"""
+
+
+from flask import Flask
+
+app = Flask("__name__")
+
+
+@app.route('/', strict_slashes=False)
+def hbnb_hello():
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return ("HBNB")
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def cweb(text):
+    return ("C {}".format(text.replace("_", " ")))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def Pweb(text="is cool"):
+    return ("Python {}".format(text.replace("_", " ")))
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def isnum(n):
+    if isinstance(n, int):
+        return ("{} is a number".format(n))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
