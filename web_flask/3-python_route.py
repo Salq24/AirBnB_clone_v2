da759e31dbb975236996e2296135db31def78605
Salq24 <qogheneochuko@gmail.com>
2024-05-31 08:17:54 -0700

    hello_route

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -26,7 +26,7 @@ def cweb(text):
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
-dey python(text="is cool"):
+dey PText(text="is cool"):
     return ("Python {}".format(text.replace("_", " ")))
 
 
